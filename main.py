e43dbccb16a89279f10c7afa00f11f11a1f806c3
Igor Alexey <igor@vortex-dev.com>
2023-10-14 15:31:05 -0300

    QOL patch

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 import os
 import sys
 import openai
+from art import *
 
 load_dotenv()
-
 
 def clear_screen():
     if sys.platform == "win32":
@@ -15,10 +15,14 @@ def clear_screen():
     else:
         os.system("clear")
 
-
 file = open("system.txt", "r")
 system_prompt = file.read()
 file.close()
+file = open("welcome.txt", "r")
+welcome_prompt = file.read()
+file.close()
+
+art = text2art("Adventure", font="small")
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -29,7 +33,7 @@ chat = [
     },
     {
         "role": "assistant",
-        "content": "Welcome to [bold cyan]Adventure[/] - The genesis of your story awaits...\n\n[bold]Disclaimer[/]: The pen is mightier than the sword, every word you inscribe may brand your path with [b red]irreversible marks[/]. May the wisdom of the ancients guide your choices.\n\nIn your words, proffer the beginning you wish for your fable",
+        "content": welcome_prompt,
     },
 ]
 
@@ -43,13 +47,12 @@ def get_chat_response():
         model="gpt-4",
         messages=chat,
         temperature=1.05,
-        max_tokens=853,
+        max_tokens=850,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
     )
     return response["choices"][0]["message"]["content"]
-
 
 def print_chat():
     for message in chat:
@@ -64,15 +67,14 @@ def print_chat():
 
 while True:
     clear_screen()
+    print(art)
     print_chat()
     action = input("\n> ")
     if action.lower() == "undo":
         if len(chat) > 2:  # Check to ensure we don't pop beyond chat length
             chat.pop()  # Remove assistant's last response
             chat.pop()  # Remove user's last message
-        else:
-            print("No actions to undo.")
     else:
         append_to_chat("user", "> " + action)
         assistant_response = get_chat_response()
-        append_to_chat("assistant", "> " + assistant_response)
+        append_to_chat("assistant", assistant_response)
